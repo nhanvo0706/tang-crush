2b6909406d206f2440bd13048fc658e38b464cf3
nhanvo0706 <86714830+nhanvo0706@users.noreply.github.com>
2021-06-30 17:09:44 +0700

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ const CONFIG = {
     btnNo: 'KHÔNG YÊU ',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh ?',
     btnReply: 'Gửi anh',
-    reply: 'Biết rồi khỏi ghi , yêu thì cần gì lí do ? =)))',
+    reply: 'Biết rồi khỏi ghi , yêu thì cần gì lí do ? =))',
     mess: 'Anh biết mà . Anh yêu em nhiều ♥ ',
     messDesc: 'Ráng qua thời gian này nha , hết dịch anh lên sì gòn lại với em . ',
     btnAccept: 'Yêu em . Hơn hôm qua ',
